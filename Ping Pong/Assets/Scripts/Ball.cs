b3d1ecb6971cf68e60a11e98b5fb881864d2bffc
Rakshit N Prabhu <prabhurakshit@gmail.com>
2023-03-27 16:12:43 +0530

    ball script updated

--- Ping Pong/Assets/Scripts/Ball.cs
+++ Ping Pong/Assets/Scripts/Ball.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] Rigidbody rb;
+    [SerializeField] Rigidbody2D rb;
     [SerializeField] float movementSpeed = 10f;
+
+    private void Start()
+    {
+        Vector2 force = new Vector2(-1f,Random.Range(-1f,1f));
+        rb.AddForce(force.normalized * movementSpeed);
+    }
 
     private void FixedUpdate()
     {
-
+        rb.velocity = rb.velocity.normalized * movementSpeed;
     }
 }
